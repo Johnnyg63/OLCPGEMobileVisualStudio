52a3a04d892063c8411f7bca5890604641a49e89
John Galvin <jgalvin@backupearth.com>
2024-06-05 16:46:08 +0100

    Updated Base files for ios.application Before trying to add PGEALLInOn.xcode project settings

--- OLCPGEMobileiOSApplication/AppDelegate.mm
+++ OLCPGEMobileiOSApplication/AppDelegate.mm
@@ -1,36 +1,70 @@
+
+//////////////////////////////////////////////////////////////////
+// Pixel Game Engine Mobile Release 2.2.6,                      //
+// John Galvin aka Johnngy63: 03-Jun-2024                       //
+// iOS Sensor NOT supported, coming soon                        //
+// Please report all bugs to https://discord.com/invite/WhwHUMV //
+// Or on Github: https://github.com/Johnnyg63					//
+//////////////////////////////////////////////////////////////////
+
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+//- (void)setIOSNativeGlue:(IOSNativeApp*)mIOSNativeApp;
 @end
 
 @implementation AppDelegate
+{
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    if(self.pIOSNativeApp == nullptr)
+    {
+        self.pIOSNativeApp = new IOSNativeApp;
+    }
+    self.pIOSNativeApp->IOSDirectEvent(IOSNativeApp::FINISHED_LAUNCH);
+    // Remove gestures, you will thank me later
+    for (UIGestureRecognizer *gr in self.window.gestureRecognizers)
+    {
+      [self.window removeGestureRecognizer:gr];
+    }
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    self.pIOSNativeApp->IOSDirectEvent(IOSNativeApp::RESIGN_ACTIVE);
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    self.pIOSNativeApp->IOSDirectEvent(IOSNativeApp::ENTERED_BACKGROUND);
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    self.pIOSNativeApp->IOSDirectEvent(IOSNativeApp::ENTERED_FOREGROUND);
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    self.pIOSNativeApp->IOSDirectEvent(IOSNativeApp::BECOME_ACTIVE);
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    self.pIOSNativeApp->IOSDirectEvent(IOSNativeApp::TERMINATE);
+    
+    // I created a standard pointer to the IOSNativeApp as I was having WAY TO MUCH headache using a smartpointer
+    // well done apple lol, therefore we need to delete the pointer and clear any memory
+    delete self.pIOSNativeApp;
+    
 }
 
 @end
--- OLCPGEMobileiOSApplication/GameViewController.h
+++ OLCPGEMobileiOSApplication/GameViewController.h
@@ -1,3 +1,12 @@
+
+//////////////////////////////////////////////////////////////////
+// Pixel Game Engine Mobile Release 2.2.6,                      //
+// John Galvin aka Johnngy63: 03-Jun-2024                       //
+// iOS Sensor NOT supported, coming soon                        //
+// Please report all bugs to https://discord.com/invite/WhwHUMV //
+// Or on Github: https://github.com/Johnnyg63					//
+//////////////////////////////////////////////////////////////////
+
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 
